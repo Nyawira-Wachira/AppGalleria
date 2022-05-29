9cd5938d117ff376a8552529f3c62de186011ad0
ABBY <abbyW@moringa.com>
2022-05-29 23:05:55 +0300

    Updated the templates.

--- photos/urls.py
+++ photos/urls.py
@@ -2,5 +2,7 @@ from django.urls import re_path
 from . import views
 
 urlpatterns=[
-    re_path(r'^$',views.home,name='newsToday'),
+    re_path(r'^$',views.gallery,name='gallery'),
+    re_path(r'^photo/(\d+)',views.photo,name ='photo')
+
 ]
