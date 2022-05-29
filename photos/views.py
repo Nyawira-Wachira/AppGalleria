933a61b4ee5532010d152390571314fddbc627e9
ABBY <abbyW@moringa.com>
2022-05-29 21:44:35 +0300

    Created view functions .

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render (request, 'gallery.html')
+
+def photo(request):
+    return render (request, 'photo.html')
