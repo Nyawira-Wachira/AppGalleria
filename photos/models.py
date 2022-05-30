0672457c2a8644dd5b228a328397afc934ec77fa
ABBY <abbyW@moringa.com>
2022-05-30 07:48:21 +0300

    Created image model and made first migrations.

--- photos/models.py
+++ photos/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Image(models.Model):
+    image = models.ImageField(null=False, blank=False)
+    image_name = models.CharField(max_length =30)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.description
+
+  
