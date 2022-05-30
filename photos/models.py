0b56ee56f70cc24b1b8a10d3d64676fbdc2cf477
ABBY <abbyW@moringa.com>
2022-05-30 08:48:57 +0300

    created the location model and updated the image model.

--- photos/models.py
+++ photos/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length =50, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length =20, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
 class Image(models.Model):
     image = models.ImageField(null=False, blank=False)
     image_name = models.CharField(max_length =30)
     description = models.TextField()
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.description
-
-  
