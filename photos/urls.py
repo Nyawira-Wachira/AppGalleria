484f6a9cd5f1d4a39baf746bbc80e47a505b401d
ABBY <abbyW@moringa.com>
2022-05-31 06:34:33 +0300

    Added the category functionality and updated the template.

--- photos/urls.py
+++ photos/urls.py
@@ -1,11 +1,14 @@
-from django.urls import re_path
+from django.urls import re_path,path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
     re_path(r'^$',views.gallery,name='gallery'),
-    re_path(r'^photo/(\d+)',views.image,name ='image')
+    re_path(r'^photo/(\d+)',views.image,name ='image'),
+    # re_path(r'^$',views.navbar,name='navbar'),
+    path('category/<int:category_Id>',views.category, name='category')
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photos/views.py
+++ photos/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
-
-import photos
 from .models import Category,Image,Location 
 
 # Create your views here.
 def gallery(request):
+    categories = Category.objects.all()
     images = Image.objects.all()
-    context = {'images':images}
-    return render (request, 'gallery.html',context)
+    # context = {'images':images}
+    return render (request, 'gallery.html', {'categories':categories,'images':images})
+
+# def navbar(request):
+#     categories = Category.objects.all()
+#     print(categories)
+#     return render (request, "navbar.html", {'categories':categories})
 
 def image(request, image_id): 
     image = Image.objects.get(id=image_id)
     return render (request, "photo.html",{"image":image})
 
-   
+def category(request, category_Id):
+    single_category =Category.objects.get(id=category_Id) 
+    images = Image.objects.filter(category=single_category)
+    return render(request,'categories/category.html',{"images":images, "category":single_category})
